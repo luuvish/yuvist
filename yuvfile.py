8036251f6b88cea5f253ad573c98b11376b7e334
luuvish <luuvish@gmail.com>
2012-12-15 03:46:04 +0900

    YuvImage YUV to RGB color conversion shader program

--- yuvfile.py
+++ yuvfile.py
@@ -32,108 +32,119 @@ VP8kUToG      = [0] * 256
 VP8kClip      = [0] * (YUV_RANGE_MAX - YUV_RANGE_MIN);
 
 def _init():
-	for i in xrange(256):
-		VP8kVToR[i] = (89858 * (i - 128) + YUV_HALF) >> YUV_FIX
-		VP8kUToG[i] = -22014 * (i - 128) + YUV_HALF
-		VP8kVToG[i] = -45773 * (i - 128)
-		VP8kUToB[i] = (113618 * (i - 128) + YUV_HALF) >> YUV_FIX
+    for i in xrange(256):
+        VP8kVToR[i] = (89858 * (i - 128) + YUV_HALF) >> YUV_FIX
+        VP8kUToG[i] = -22014 * (i - 128) + YUV_HALF
+        VP8kVToG[i] = -45773 * (i - 128)
+        VP8kUToB[i] = (113618 * (i - 128) + YUV_HALF) >> YUV_FIX
 
-	for i in xrange(YUV_RANGE_MIN, YUV_RANGE_MAX):
-		k = ((i - 16) * 76283 + YUV_HALF) >> YUV_FIX
-		k = 0 if k < 0 else 255 if k > 255 else k
-		VP8kClip[i - YUV_RANGE_MIN] = k
+    for i in xrange(YUV_RANGE_MIN, YUV_RANGE_MAX):
+        k = ((i - 16) * 76283 + YUV_HALF) >> YUV_FIX
+        k = 0 if k < 0 else 255 if k > 255 else k
+        VP8kClip[i - YUV_RANGE_MIN] = k
 
 _init()
 
 
 class YuvFile(object):
 
-	chroma = {
-		'yuv'   : (2,2),
-		'yuv400': (1,1),
-		'yuv420': (2,2),
-		'yuv422': (1,2),
-		'yuv224': (2,1),
-		'yuv444': (1,1)
-	}
+    chroma = {
+        'yuv'   : (2,2),
+        'yuv400': (1,1),
+        'yuv420': (2,2),
+        'yuv422': (1,2),
+        'yuv224': (2,1),
+        'yuv444': (1,1)
+    }
 
-	def __init__(self, filename, size, format='yuv'):
-		try:
-			self.file = open(filename, 'rb')
-		except IOError:
-			raise Exception("Can't open file %s" % filename)
-		self.size   = size
-		self.format = format
+    def __init__(self, filename, size, format='yuv'):
+        try:
+            self.file = open(filename, 'rb')
+        except IOError:
+            raise Exception("Can't open file %s" % filename)
+        self.size   = size
+        self.format = format
 
-		self.oneframe = self.size[0] * self.size[1] * 3 / 2
+        self.oneframe = self.size[0] * self.size[1] * 3 / 2
 
-		self.filesize = os.path.getsize(filename)
-		self.duration = self.filesize // self.oneframe
-		self.position = 0
+        self.filesize = os.path.getsize(filename)
+        self.duration = self.filesize // self.oneframe
+        self.position = 0
 
-		if format not in YuvFile.chroma:
-			raise Exception("Not support chroma format")
-		self.scale = YuvFile.chroma[format]
+        if format not in YuvFile.chroma:
+            raise Exception("Not support chroma format")
+        self.scale = YuvFile.chroma[format]
 
-	def close(self):
-		if self.file:
-			self.file.close()
+    def close(self):
+        if self.file:
+            self.file.close()
 
-	def read(self, position=None):
-		if position:
-			self.seek(position)
-		buf = self.file.read(self.oneframe)
-		buf = self.YUVtoRGB(buf)
-		buf = ''.join(map(chr, buf))
-		return buf
+    def read(self, position=None):
+        if position:
+            self.seek(position)
+        if self.position >= self.duration:
+            self.seek(0)
 
-	def seek(self, position):
-		self.file.seek(position * self.oneframe, os.SEEK_SET)
+        y = self.file.read(self.size[0] * self.size[1])
+        u = self.file.read(self.size[0] * self.size[1] / 4)
+        v = self.file.read(self.size[0] * self.size[1] / 4)
+        self.position += 1
+        return y, u, v
 
-	def YUVtoRGB(self, yuvs):
-		def raster(yuvs):
-			format = self.format
-			size   = self.size
-			scale  = self.scale
-			ubase  = size[0] * size[1]
-			vbase  = ubase + (size[0] // scale[0]) * (size[1] // scale[1])
-			cwidth = size[0] // scale[0]
-			y, u, v = 128, 128, 128
-			for posy in xrange(size[1]):
-				for posx in xrange(size[0]):
-					y = yuvs[size[0] * posy + posx]
-					if format != 'yuv400':
-						u = yuvs[ubase + cwidth * (posy // scale[1]) + (posx // scale[0])]
-						v = yuvs[vbase + cwidth * (posy // scale[1]) + (posx // scale[0])]
-					yield ord(y), ord(u), ord(v)
+        buf = self.file.read(self.oneframe)
+        buf = self.YUVtoRGB(buf)
+        buf = ''.join(map(chr, buf))
+        return buf
 
-		clip = lambda x: min(max(0, x), 255)
-		def rgb(y, u, v):
-			r = 1.164 * (y-16)                   + 1.596 * (v-128)
-			g = 1.164 * (y-16) - 0.391 * (u-128) - 0.813 * (v-128)
-			b = 1.164 * (y-16) + 2.018 * (u-128)
-			return clip(int(r)), clip(int(g)), clip(int(b))
+    def seek(self, position):
+        if position < self.duration:
+            self.position = position
+            self.file.seek(position * self.oneframe, os.SEEK_SET)
 
-		def rgb2(y, u, v):
-			r = 298 * (y-16)                 + 409 * (v-128)
-			g = 298 * (y-16) - 100 * (u-128) - 208 * (v-128)
-			b = 298 * (y-16) + 516 * (u-128)
-			return clip((r+128)>>8), clip((g+128)>>8), clip((b+128)>>8)
+    def YUVtoRGB(self, yuvs):
+        def raster(yuvs):
+            format = self.format
+            size   = self.size
+            scale  = self.scale
+            ubase  = size[0] * size[1]
+            vbase  = ubase + (size[0] // scale[0]) * (size[1] // scale[1])
+            cwidth = size[0] // scale[0]
+            y, u, v = 128, 128, 128
+            for posy in xrange(size[1]):
+                for posx in xrange(size[0]):
+                    y = yuvs[size[0] * posy + posx]
+                    if format != 'yuv400':
+                        u = yuvs[ubase + cwidth * (posy // scale[1]) + (posx // scale[0])]
+                        v = yuvs[vbase + cwidth * (posy // scale[1]) + (posx // scale[0])]
+                    yield ord(y), ord(u), ord(v)
 
-		def rgb3(y, u, v):
-			r_off = VP8kVToR[v]
-			g_off = (VP8kVToG[v] + VP8kUToG[u]) >> YUV_FIX
-			b_off = VP8kUToB[u]
+        clip = lambda x: min(max(0, x), 255)
+        def rgb(y, u, v):
+            r = 1.164 * (y-16)                   + 1.596 * (v-128)
+            g = 1.164 * (y-16) - 0.391 * (u-128) - 0.813 * (v-128)
+            b = 1.164 * (y-16) + 2.018 * (u-128)
+            return clip(int(r)), clip(int(g)), clip(int(b))
 
-			r = VP8kClip[y + r_off - YUV_RANGE_MIN]
-			g = VP8kClip[y + g_off - YUV_RANGE_MIN]
-			b = VP8kClip[y + b_off - YUV_RANGE_MIN]
-			return r, g, b
+        def rgb2(y, u, v):
+            r = 298 * (y-16)                 + 409 * (v-128)
+            g = 298 * (y-16) - 100 * (u-128) - 208 * (v-128)
+            b = 298 * (y-16) + 516 * (u-128)
+            return clip((r+128)>>8), clip((g+128)>>8), clip((b+128)>>8)
 
-		rgbs = []
-		for y, u, v in raster(yuvs):
-			r, g, b = rgb3(y, u, v)
-			rgbs.append(r)
-			rgbs.append(g)
-			rgbs.append(b)
-		return rgbs
+        def rgb3(y, u, v):
+            r_off = VP8kVToR[v]
+            g_off = (VP8kVToG[v] + VP8kUToG[u]) >> YUV_FIX
+            b_off = VP8kUToB[u]
+
+            r = VP8kClip[y + r_off - YUV_RANGE_MIN]
+            g = VP8kClip[y + g_off - YUV_RANGE_MIN]
+            b = VP8kClip[y + b_off - YUV_RANGE_MIN]
+            return r, g, b
+
+        rgbs = []
+        for y, u, v in raster(yuvs):
+            r, g, b = rgb3(y, u, v)
+            rgbs.append(r)
+            rgbs.append(g)
+            rgbs.append(b)
+        return rgbs
--- yuvimage.py
+++ yuvimage.py
@@ -20,75 +20,146 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from threading import Lock
-from kivy.cache import Cache
 from kivy.clock import Clock
+from kivy.lang import Builder
 from kivy.resources import resource_find
+from kivy.graphics import RenderContext
 from kivy.graphics.texture import Texture
-from kivy.uix.image import Image
+from kivy.properties import StringProperty, ObjectProperty
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
 
 import sys
 sys.path.insert(0, '..')
 from yuvfile import YuvFile
 
 
-class YuvImage(Image):
+Builder.load_string('''
+<YuvImage>:
+    canvas:
+        Color:
+            rgb: 1, 1, 1
+        BindTexture:
+            texture: self.texture2
+            index: 2
+        BindTexture:
+            texture: self.texture1
+            index: 1
+        Rectangle:
+            texture: self.texture0
+            pos: self.pos
+            size: self.size
+''')
 
-	def __init__(self, **kwargs):
-		self._buffer_lock = Lock()
-		self._buffer = None
-		super(Image, self).__init__(**kwargs)
-		self.bind(source=self.texture_update, mipmap=self.texture_update)
-		if self.source:
-			self.texture_update()
+class YuvImage(Widget):
 
-		Clock.schedule_interval(self._update, 1 / 30.)
+    CONVERT_YUV_RGB_FS = '''$HEADER$
+    uniform sampler2D texture1;
+    uniform sampler2D texture2;
 
-	def texture_update(self, *largs):
-		if not self.source:
-			self.texture = None
-		else:
-			filename = resource_find(self.source)
-			if filename is None:
-				return
-			self.im = YuvFile(filename, size=(1920,1080), format='yuv')
-			self._buffer = self.im.read()
-			self._update(None)
+    void main(void) {   
+        float r, g, b, y, u, v;
+        y = texture2D(texture0, tex_coord0).s;
+        u = texture2D(texture1, tex_coord0).s;
+        v = texture2D(texture2, tex_coord0).s;
 
-	def _populate_texture(self, texture):
-		texture.flip_vertical()
-		texture.blit_buffer(self._buffer, size=self.im.size, colorfmt='rgb')
+        y = 1.1643 * (y - 0.0625);
+        u = u - 0.5;
+        v = v - 0.5;
 
-	def _update_texture(self, buf):
-		texture = Texture.create(size=self.im.size, colorfmt='rgb')
-		texture.add_reload_observer(self._populate_texture)
-		self._populate_texture(texture)
-		self.texture = texture
+        r = y + 1.5958  * v;
+        g = y - 0.39173 * u - 0.81290 * v;
+        b = y + 2.017   * u;
 
-	def _update(self, dt):
-		with self._buffer_lock:
-			if self._buffer is not None:
-				self._update_texture(self._buffer)
-				self._buffer = None
-				self._buffer = self.im.read()
+        gl_FragColor = vec4(r, g, b, 1.0);
+    }
+    '''
+    fs = StringProperty(None)
 
-	def reload(self):
-		uid = '%s|%s|%s' % (self.source, self.mipmap, 0)
-		Cache.remove("kv.texture", uid)
-		olsource = self.source
-		self.source = ''
-		self.source = olsource
+    source = StringProperty(None)
+
+    texture0 = ObjectProperty(None, allownone=True)
+    texture1 = ObjectProperty(None, allownone=True)
+    texture2 = ObjectProperty(None, allownone=True)
+
+    def __init__(self, **kwargs):
+        self._buffer_lock = Lock()
+        self._buffer = None
+        self.canvas = RenderContext(fs=self.CONVERT_YUV_RGB_FS)
+        self.canvas['projection_mat'] = Window.render_context['projection_mat']
+        self.canvas['modelview_mat']  = Window.render_context['modelview_mat']
+        self.canvas['texture1'] = 1
+        self.canvas['texture2'] = 2
+        super(YuvImage, self).__init__(**kwargs)
+        self.bind(source=self.texture_update)
+        if self.source:
+            self.texture_update()
+        Clock.schedule_interval(self.update_glsl, 1 / 30.)
+
+    def on_fs(self, instance, value):
+        shader = self.canvas.shader
+        old_value = shader.fs
+        shader.fs = value
+        if not shader.success:
+            shader.fs = old_value
+            raise Exception('failed')
+
+    def texture_update(self, *largs):
+        if not self.source:
+            self.texture = None
+        else:
+            filename = resource_find(self.source)
+            if filename is None:
+                return
+
+            self.im = YuvFile(filename, size=(1920,1080), format='yuv')
+            self._buffer = self.im.read()
+            self.update_glsl(None)
+
+    def _populate_texture_y(self, texture):
+        texture.flip_vertical()
+        texture.blit_buffer(self._buffer[0], size=(self.im.size[0]/1,self.im.size[1]/1), colorfmt='luminance')
+    def _populate_texture_u(self, texture):
+        texture.flip_vertical()
+        texture.blit_buffer(self._buffer[1], size=(self.im.size[0]/2,self.im.size[1]/2), colorfmt='luminance')
+    def _populate_texture_v(self, texture):
+        texture.flip_vertical()
+        texture.blit_buffer(self._buffer[2], size=(self.im.size[0]/2,self.im.size[1]/2), colorfmt='luminance')
+
+    def _update_texture(self, buf):
+        y_texture = Texture.create(size=(self.im.size[0]/1,self.im.size[1]/1), colorfmt='luminance')
+        u_texture = Texture.create(size=(self.im.size[0]/2,self.im.size[1]/2), colorfmt='luminance')
+        v_texture = Texture.create(size=(self.im.size[0]/2,self.im.size[1]/2), colorfmt='luminance')
+        y_texture.add_reload_observer(self._populate_texture_y)
+        u_texture.add_reload_observer(self._populate_texture_u)
+        v_texture.add_reload_observer(self._populate_texture_v)
+        self._populate_texture_y(y_texture)
+        self._populate_texture_u(u_texture)
+        self._populate_texture_v(v_texture)
+        self.texture0 = y_texture
+        self.texture1 = u_texture
+        self.texture2 = v_texture
+
+    def update_glsl(self, dt):
+        with self._buffer_lock:
+            if self._buffer is not None:
+                self._update_texture(self._buffer)
+                self._buffer = None
+                self._buffer = self.im.read()
+
+                print('FPS: %2.4f (real draw: %d)' % (Clock.get_fps(), Clock.get_rfps()))
 
 
 if __name__ == '__main__':
-	from kivy.app import App
-	import sys
+    from kivy.app import App
+    import sys
 
-	if len(sys.argv) != 2:
-		print "usage: %s file" % sys.argv[0]
-		sys.exit(1)
+    if len(sys.argv) != 2:
+        print "usage: %s file" % sys.argv[0]
+        sys.exit(1)
 
-	class YuvImageApp(App):
-		def build(self):
-			return YuvImage(source=sys.argv[1])
+    class YuvImageApp(App):
+        def build(self):
+            return YuvImage(source=sys.argv[1])
 
-	YuvImageApp().run()
+    YuvImageApp().run()
