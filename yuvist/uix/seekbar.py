1add77129f8ccefb21e51a695d91122ce81f1887
luuvish <luuvish@gmail.com>
2013-02-17 04:23:29 +0900

    seekbar is inherited from VideoPlayerProgressBar

--- yuvist/uix/seekbar.py
+++ yuvist/uix/seekbar.py
@@ -20,74 +20,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __all__ = ('SeekBar', )
 
-from kivy.uix.progressbar import ProgressBar
-from kivy.properties import NumericProperty, ObjectProperty
-from kivy.animation import Animation
-from kivy.factory import Factory
+from kivy.uix.videoplayer import VideoPlayerProgressBar
 
 
-class SeekBar(ProgressBar):
-
-    video = ObjectProperty(None)
-    seek  = NumericProperty(None, allownone=True)
-    alpha = NumericProperty(1.)
+class SeekBar(VideoPlayerProgressBar):
 
     def __init__(self, **kwargs):
 
         super(SeekBar, self).__init__(**kwargs)
 
-        self.bubble = Factory.Bubble(size=(72,44))
-        self.label = Factory.Label(text='0:00')
-        self.bubble.add_widget(self.label)
-        self.add_widget(self.bubble)
-        self.bind(pos=self._update_bubble,
-                  size=self._update_bubble,
-                  seek=self._update_bubble)
-
-    def on_video(self, instance, value):
-        self.video.bind(position=self._update_bubble, state=self._showhide_bubble)
-
-    def on_touch_down(self, touch):
-        if not self.collide_point(*touch.pos):
-            return
-        self._show_bubble()
-        touch.grab(self)
-        self._update_seek(touch.x)
-        return True
-
-    def on_touch_move(self, touch):
-        if touch.grab_current is not self:
-            return
-        self._update_seek(touch.x)
-        return True
-
-    def on_touch_up(self, touch):
-        if touch.grab_current is not self:
-            return
-        touch.ungrab(self)
-        if self.seek:
-            self.video.seek(self.seek)
-        self.seek = None
-        self._hide_bubble()
-        return True
-
-    def _update_seek(self, x):
-        if self.width == 0:
-            return
-        x = max(self.x, min(self.right, x)) - self.x
-        self.seek = x / float(self.width)
-
-    def _show_bubble(self):
-        self.alpha = 1.
-        Animation.stop_all(self, 'alpha')
-
-    def _hide_bubble(self):
-        self.alpha = 1.
-        Animation(alpha=0, d=4, t='in_out_expo').start(self)
-
-    def on_alpha(self, instance, value):
-        self.bubble.background_color = (1, 1, 1, value)
-        self.label.color = (1, 1, 1, value)
+        self.bubble.size = (72,44)
 
     def _update_bubble(self, *l):
         seek = self.seek
@@ -100,12 +42,6 @@ class SeekBar(ProgressBar):
         hours   = int(d / 3600)
         minutes = int(d / 60) - (hours * 60)
         seconds = int(d) - (hours * 3600 + minutes * 60)
-        self.label.text = '%02d:%02d:%02d' % (hours, minutes, seconds)
+        self.bubble_label.text = '%02d:%02d:%02d' % (hours, minutes, seconds)
         self.bubble.center_x = self.x + seek * self.width
         self.bubble.y = self.top
-
-    def _showhide_bubble(self, instance, value):
-        if value == 'play':
-            self._hide_bubble()
-        else:
-            self._show_bubble()
